eb2194822443cbb1ec04b36c722a31c1e1635f99
Carl 'Subzidion' Hiltbrunner <git@carl.hiltbrunner.email>
2024-08-09 17:58:12 -0700

    Initial Commit

---
+++ ElectionDrop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElectionDrop
+//
+//  Created by Carl Hiltbrunner on 8/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElectionDrop/ElectionDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectionDropApp.swift
+//  ElectionDrop
+//
+//  Created by Carl Hiltbrunner on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ElectionDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
